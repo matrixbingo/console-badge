73274bfc0f72e0f2688f8c13e3b3b8e9bb638685
Dennis Herzberg <dennishzg@gmail.com>
2019-08-08 15:35:34 +0200

    refactor: remove babel, write ES5 only code using transpilers will only increase the overall bundle size

--- src/index.js
+++ src/index.js
@@ -1,46 +1,76 @@
 'use strict';
 
 export function log (options) {
-  options = {
+  options = typeof options !== 'undefined' ? options : {};
+
+  var optionsDefaults = {
     mode: 'default',
     leftText: 'Lorem',
     rightText: 'Ipsum',
     leftTextColor: '#ffffff',
     rightTextColor: '#ffffff',
     leftBgColor: '#555555',
-    rightBgColor: '#0366d6',
-    ...options
+    rightBgColor: '#0366d6'
   };
 
-  const badge = () => {
-    const agent = navigator.userAgent.toLowerCase();
+  var prop;
 
-    if (/(chrome|firefox|safari)/.test(agent.toLowerCase())) {
-      const leftBoxStyle = [
-        `background-color: ${options.leftBgColor}`,
-        ...(options.mode === 'shields.io' ? ['padding: 6px 4px 6px 6px'] : ['padding: 16px 10px 16px 10px']),
-        ...(options.mode === 'shields.io' ? ['border-radius: 3px 0 0 3px'] : []),
-        ...(options.mode === 'shields.io' ? ['font-family: DejaVu Sans,Verdana,Geneva,sans-serif'] : []),
-        `color: ${options.leftTextColor}`,
-        ...(options.mode === 'shields.io' ? ['text-shadow: 0 1px 0 rgba(1, 1, 1, 0.3)'] : [])
-      ].join(';');
-
-      const rightBoxStyle = [
-        `background-color: ${options.rightBgColor}`,
-        ...(options.mode === 'shields.io' ? ['padding: 6px 6px 6px 4px'] : ['padding: 16px 10px 16px 10px']),
-        ...(options.mode === 'shields.io' ? ['border-radius: 0 3px 3px 0'] : []),
-        ...(options.mode === 'shields.io' ? ['font-family: DejaVu Sans,Verdana,Geneva,sans-serif'] : []),
-        `color: ${options.rightTextColor}`,
-        ...(options.mode === 'shields.io' ? ['text-shadow: 0 1px 0 rgba(1, 1, 1, 0.3)'] : [])
-      ].join(';');
-
-      const spacerStyle = ['background-color: transparent'].join(';');
-
-      console.log(`\n\n %c ${options.leftText} %c ${options.rightText} %c \n\n\n`, leftBoxStyle, rightBoxStyle, spacerStyle);
-    } else {
-      window.console && console.log(`${options.leftText} - ${options.rightText}`);
+  for (prop in optionsDefaults) {
+    if (!(prop in options)) {
+      options[prop] = optionsDefaults[prop];
     }
-  };
+  }
 
-  return badge();
+  var agent = navigator.userAgent.toLowerCase();
+
+  if (/(chrome|firefox|safari)/.test(agent)) {
+    var leftBoxStyle = [
+      'background-color: ' + options.leftBgColor,
+      'color: ' + options.leftTextColor
+    ];
+
+    var rightBoxStyle = [
+      'background-color: ' + options.rightBgColor,
+      'color: ' + options.rightTextColor
+    ];
+
+    var shieldsioFontFamily = 'font-family: DejaVu Sans,Verdana,Geneva,sans-serif';
+    var shieldsioTextShadow = 'text-shadow: 0 1px 0 rgba(1, 1, 1, 0.3)';
+    var defaultPadding = 'padding: 16px 10px 16px 10px';
+
+    if (options.mode === 'shields.io') {
+      leftBoxStyle.push(
+        'padding: 6px 4px 6px 6px',
+        'border-radius: 3px 0 0 3px',
+        shieldsioFontFamily,
+        shieldsioTextShadow
+      );
+
+      rightBoxStyle.push(
+        'padding: 6px 6px 6px 4px',
+        'border-radius: 0 3px 3px 0',
+        shieldsioFontFamily,
+        shieldsioTextShadow
+      );
+    } else {
+      leftBoxStyle.push(
+        defaultPadding
+      );
+
+      rightBoxStyle.push(
+        defaultPadding
+      );
+    }
+
+    leftBoxStyle = leftBoxStyle.join(';');
+    rightBoxStyle = rightBoxStyle.join(';');
+
+    var spacerStyle = ['background-color: transparent'].join(';');
+
+    // eslint-disable-next-line no-console
+    console.log('\n\n %c ' + options.leftText + ' %c ' + options.rightText + ' %c \n\n\n', leftBoxStyle, rightBoxStyle, spacerStyle);
+  } else {
+    // eslint-disable-next-line no-console
+    window.console && console.log(options.leftText + ' - ' + options.rightText);
+  }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -25,17 +25,6 @@ module.exports = {
       })
     ]
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  },
   plugins: [
     new webpack.BannerPlugin({
       banner: [
